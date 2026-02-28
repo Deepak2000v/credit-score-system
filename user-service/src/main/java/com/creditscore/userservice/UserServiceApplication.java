a5510f8485e83a58def3ae72bb2d89d2a3237ec6
Deepak kumar <shadydeepak2000@gmail.com>
2026-02-28 12:06:09 +0530

    Initialize user service with springboot

--- user-service/src/main/java/com/creditscore/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/creditscore/userservice/UserServiceApplication.java
@@ -1,4 +1,4 @@
-package com.creditscore.user_service;
+package com.creditscore.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
